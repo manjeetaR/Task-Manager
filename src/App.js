f8b6173a7e5364779757bed8776a0b68144e2bde
manjeeta <manjeetaraj@gmail.com>
2019-10-02 21:47:47 +0530

    dummy data to get started

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { Component, Fragment } from 'react'
 import { initialize, dataModule } from './initialize'
 
-initialize();
+// initialize();
 
-const posts = dataModule.dataset("tasks");
+// const posts = dataModule.dataset("tasks")
+
+const dummyList = [{
+  id: 1,
+  name: "Meeting",
+  deadline: "2019-08-02"
+}]
 
 class App extends Component {
 
@@ -19,24 +25,32 @@ class App extends Component {
 
   addTask = async () => {
     const { name, list } = this.state
-    const subList = await posts.insert([{ name }]).execute()
-    if (list.length > 0) this.setState({ list: [...list, ...subList] })
+    // const subList = await posts.insert([{ name }]).execute()
+    const subList = [{
+      id: Math.random(),
+      name,
+      deadline: ""
+    }]
+    if (subList.length > 0) this.setState({ list: [...list, ...subList] })
   }
 
   getTasks = async () => {
-    let list = await posts.select().execute()
+    // const list = await posts.select().execute()
+    const list = dummyList
     if (list.length > 0) this.setState({ list })
   }
 
   deleteTask = async id => {
     const { list } = this.state
-    let deletedList = await posts.delete().where(field => field("id").isLike(id)).execute()
+    // const deletedList = await posts.delete().where(field => field("id").isLike(id)).execute()
+    const deletedList = list.filter(item => item.id === id)
     if (deletedList.length > 0) this.setState({ list: list.filter(item => item.id !== deletedList[0].id) })
   }
 
   updateTask = async (id, name, deadline) => {
     const { list } = this.state
-    let updatedList = await posts.update([{ name, deadline }]).where(field => field("id").isLike(id)).execute()
+    // const updatedList = await posts.update([{ name, deadline }]).where(field => field("id").isLike(id)).execute()
+    const updatedList = [{ id, name, deadline }]
     if (updatedList.length > 0) this.setState({ list: list.map(item => item.id === updatedList[0].id ? updatedList[0] : item) })
   }
 
@@ -57,8 +71,8 @@ class App extends Component {
         <button onClick={this.addTask}>Add</button>
         <ol>
           {list.map(item =>
-            <li><input onChange={e => this.setState({ list: list.map(value => value.id === item.id ? { ...value, name: e.target.value } : value) })} defaultValue={item.name} />
-              {(item.deadline || deadline === item.id) ? <input defaultValue={item.deadline} onChange={e => this.setState({ list: list.map(value => value.id === item.id ? { ...value, deadline: e.target.value } : value) })} type="date" defaultValue={item.deadline} /> :
+            <li><input onChange={e => this.setState({ list: list.map(value => value.id === item.id ? { ...value, name: e.target.value } : value) })} value={item.name} />
+              {(item.deadline || deadline === item.id) ? <input value={item.deadline} onChange={e => this.setState({ list: list.map(value => value.id === item.id ? { ...value, deadline: e.target.value } : value) })} type="date" defaultValue={item.deadline} /> :
                 <button onClick={() => this.setState({ deadline: item.id })}>Add Deadline</button>}
               <button onClick={() => this.update(item.id)}>Update</button>
               <button style={{ color: 'red' }} onClick={() => this.deleteTask(item.id)}>X</button></li>
