b5aa3fb4001bedf5121b728edf838fe4212a4aca
JavaScriptErika <erikajritter@gmail.com>
2019-10-03 15:05:27 -0400

    fix spelling and grammar errors :)

--- src/Alert.js
+++ src/Alert.js
@@ -5,13 +5,13 @@ class Alert extends React.Component {
     let text;
     switch(this.props.type) {
       case 'update': 
-        text = this.props.success ? 'Updated task list successfully.' : 'An error ocurred while updating task list.'; 
+        text = this.props.success ? 'Updated task list successfully.' : 'An error occurred while updating task list.'; 
         break;
       case 'add': 
-        text = this.props.success ? 'Added item to task list successfully.' : 'An error ocurred while adding item to task list.'; 
+        text = this.props.success ? 'Added item to task list successfully.' : 'An error occurred while adding item to task list.'; 
         break;
       case 'delete': 
-        text = this.props.success ? 'Deleted item form task list successfully.' : 'An error ocurred while deleting item from task list.'; 
+        text = this.props.success ? 'Deleted item from task list successfully.' : 'An error occurred while deleting item from task list.'; 
         break;
       default: text = ''; break;
     }
