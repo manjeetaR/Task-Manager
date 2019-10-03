063d453d6a01ebccfa351a2302548775e1dbdabf
Caley Woods <caley@vcreativeinc.com>
2019-10-02 20:25:17 -0500

    Cleanup whitespace

--- src/Alert.js
+++ src/Alert.js
@@ -4,14 +4,14 @@ class Alert extends React.Component {
   getText = () => {
     let text;
     switch(this.props.type) {
-      case 'update': 
-        text = this.props.success ? 'Updated task list successfully.' : 'An error ocurred while updating task list.'; 
+      case 'update':
+        text = this.props.success ? 'Updated task list successfully.' : 'An error ocurred while updating task list.';
         break;
-      case 'add': 
-        text = this.props.success ? 'Added item to task list successfully.' : 'An error ocurred while adding item to task list.'; 
+      case 'add':
+        text = this.props.success ? 'Added item to task list successfully.' : 'An error ocurred while adding item to task list.';
         break;
-      case 'delete': 
-        text = this.props.success ? 'Deleted item form task list successfully.' : 'An error ocurred while deleting item from task list.'; 
+      case 'delete':
+        text = this.props.success ? 'Deleted item form task list successfully.' : 'An error ocurred while deleting item from task list.';
         break;
       default: text = ''; break;
     }
